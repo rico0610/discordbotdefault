0897f80eaeb29f51c83197d483d1432b79b465e3
Rico Collina <rico.collina@Ricos-Laptop.local>
2022-12-21 11:15:14 +0800

    Added ban command

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,34 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+//const wait = require('node:timers/promises').setTimeout;
+
 module.exports = {
+
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('pong!');
+		// const message = await interaction.fetchReply(); //-- FOR FETCHING THE REPLY ---
+		// console.log(message.content);
+		// await interaction.deleteReply(); // --- FOR DELETING REPLY ---
+		// await wait(2000);
+		// await interaction.followUp('Pong again!'); // --- FOR SENDING FOLLOW UP MESSAGE. EPHEMERAL CAN BE ADDED HERE AS WELL ---
+		// await interaction.editReply({ content: 'Secret Pong!', ephemeral: true }); //--- EDITING RESPONSE ---
+			//await interaction.reply({ content: 'Secret Pong!', ephemeral: true }); //--- HIDE RESPONSE EXCEPT TO EXECUTOR OF THE SLASH COMMAND ---
+
+	//-- IF COMMANDS PERFORM LONGER TASKS (MORE THAN 3 SECONDS), USE THIS --
+
+		// await interaction.deferReply(); //--- EPHEMERAL CAN BE ADDED HERE AS WELL ---
+		// await wait(4000);
+		// await interaction.editReply('Pong!');
+
+	//-- FOR LOCALIZED RESPONSES --
+		// const locales = {
+		// 	pl: 'Witaj Świecie!',
+		// 	de: 'Hallo Welt!',
+		// };
+		// interaction.reply(locales[interaction.locale] ?? 'Hello World (default is english)');
 	},
+	
 };
