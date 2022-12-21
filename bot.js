55faacd17cdc5f6afb10d81cdbd17e45a08dc18f
Rico Collina <rico.collina@Ricos-Laptop.local>
2022-12-21 09:20:46 +0800

    Added event folder

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+//---- for dynamically retrieving command files ----
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
@@ -23,28 +24,23 @@ for (const file of commandFiles) {
 	}
 }
 
+//---- for dynamically retreiving event files ----
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
+}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
+
+
+
 
 
 
